f235643febc8dece06b2c00cf4ecadd112d61b5f
Wende Lu <wendelu1991@gmail.com>
2020-03-29 11:50:17 +0800

    init

---
+++ test/ruby_example_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class RubyExampleTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::RubyExample::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
